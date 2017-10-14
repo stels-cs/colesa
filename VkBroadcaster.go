97e481abdcc08854c7c27b1872ff7b416c1ce977
Иван Недзвецкий <stels-cs@yandex.ru>
2017-10-14 07:47:48 +0300

    Fix working with broadcast API

--- VkBroadcaster.go
+++ VkBroadcaster.go
@@ -4,20 +4,47 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"io/ioutil"
+	"strings"
 )
 
 type VkBroadcaster struct {
 	url string
 }
 
+type VkBroadcasterError struct {
+	message string
+}
+
+func (err *VkBroadcasterError) Error() string {
+	return err.message
+}
+
 func (vk *VkBroadcaster) broadcast(event BroadcastEvent) error {
-	_, err := http.PostForm(vk.url,
+	data, err := http.PostForm(vk.url,
 		url.Values{
-			"message.message" : {event.message},
+			"message[message]" : {event.message},
 			"list_ids[]" : {strconv.Itoa(event.listId)},
+			"run_now": {"1"},
 		},
 	)
-	return err
+	if err != nil {
+		return err
+	}
+	defer data.Body.Close()
+
+	json, err := ioutil.ReadAll(data.Body)
+
+	if err != nil {
+		return err
+	}
+
+	if strings.Index( string(json), "response" ) != -1 {
+		return nil
+	} else {
+		return &VkBroadcasterError{"Bad response: " + string(json) }
+	}
+
 }
 
 func InitVkBroadcaster(url string) *VkBroadcaster {
